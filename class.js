cbbbccd068911d6f248777b8e4eafea22fd99a32
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-05-19 16:51:49 +0600

    end class

--- class.js
+++ class.js
@@ -36,3 +36,57 @@
 // console.log();
 
 
+// class Product {
+//     country = 'Bangladesh'
+
+//     nurani(about) {
+//         console.log(`taking about  ${about}`);
+//     }
+// }
+// const laptop = new Product();
+
+// laptop.nurani(' Eeb devloper');
+
+// laptop.nurani('House wife');
+
+// laptop.nurani('Members of PQ');
+
+// class Interduction {
+
+//     constructor(name, hscYear, institute) {
+//         this.nam = name;
+//         this.hs = hscYear;
+//         this.ins = institute;
+
+//     }
+// }
+
+// const info = new Interduction('Tanjila', 2021, 'BM Collage',)
+// console.log(info);
+
+class Vehicle {
+
+    constructor(brk, speed) {
+        this.brk = brk;
+        this.speed = speed;
+
+    }
+}
+
+// class Bus extends Vehicle 
+class Motorcycle extends Vehicle {
+    constructor(brk, speed, wheel, seat, fluetank) {
+        super(brk, speed)
+        this.wheel = wheel;
+        this.seat = seat;
+        this.seat = seat;
+        this.fluetank = fluetank;
+    }
+
+}
+
+// const bus = new Motorcycle(true, true, 4, 40);
+const motor = new Motorcycle(true, true, 4, 40, 25)
+console.log(motor);
+
+// console.log(bus);
