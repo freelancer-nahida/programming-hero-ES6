088fec74b4baf7584e2f68b4ca77df4db1b08f84
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-05-20 05:42:25 +0600

    add notionjs

---
+++ notion.js
@@ -0,0 +1,31 @@
+// class person {
+
+//     constructor(name, age) {
+
+//         this.name = name;
+//         this.age = age;
+//     }
+//     sleep() {
+
+//         console.log(`Sleeping now ${this.name}`);
+
+//     }
+// }
+
+// const kodom = new person('kodom ali', 21)
+// console.log(kodom);
+
+// const badam = new person('badam ali', 21)
+
+// console.log(badam);
+
+const person = {
+    name: 'kodom Ali',
+    3: 'sleeping'
+
+}
+console.log(person.job);
+console.log(person['3']);
+
+
+
