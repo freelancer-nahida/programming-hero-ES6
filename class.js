477d7877485eec632ad472d6dcd5d296fb817ea3
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-05-19 09:31:57 +0600

    add class js

---
+++ class.js
@@ -0,0 +1,38 @@
+// class Product {
+//     country = 'Bangladesh';
+//     speak(talk) {
+//         console.log(`talking about  ${talk}`);
+//     }
+
+
+// }
+// const laptop = new Product();
+// console.log(laptop);
+// laptop.speak('kemon aso');
+
+
+// class Product {
+//     country = 'Bangladesh';
+//     nahida(about) {
+//         console.log(`taking about ${about}`)
+
+//     }
+// }
+// console.log(new Product().nahida('Web devloper'));
+
+
+// class Teacher {
+//     lecture(subject) {
+//         console.log(`Sir is Teachring ${subject}`);
+
+//     }
+// }
+
+// const teachers = new Teacher();
+// teachers.lecture('math');
+// teachers.lecture('Science');
+// teachers.lecture('chemistry')
+
+// console.log();
+
+
